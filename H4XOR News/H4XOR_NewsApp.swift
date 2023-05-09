bcdeaf506a566b3c94264cd1f850f752bbe8fe01
Alex Asichka <sanechka3@mail.ru>
2023-05-09 13:12:35 +0300

    Initial Commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Aleksandra Asichka on 2023-05-09.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
